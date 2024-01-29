cb7ed87de709b00c036df0e4b94147594261f85a
Yidi Liu <yiliu@c1r9s4.42tokyo.jp>
2024-01-29 20:18:37 +0900

    20240129

---
+++ rush00/rush00.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiliu <yiliu@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 15:34:20 by lmaruyam          #+#    #+#             */
+/*   Updated: 2024/01/29 20:16:18 by yiliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < y)
+	{
+		j = 0;
+		while (j < x)
+		{
+			if ((i == 0 || i == y - 1) && (j == 0 || j == x - 1))
+				ft_putchar('o');
+			else if (i == 0 || i == y - 1)
+				ft_putchar('-');
+			else if (j == 0 || j == x - 1)
+				ft_putchar('|');
+			else if ((j == 0 || j == x - 1) && i == y - 1)
+				ft_putchar('o');
+			else
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
